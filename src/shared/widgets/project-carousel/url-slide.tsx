2b0369974918d779e1762f86474c2b1d9d50e1d9
Dexight <tupikin@sfedu.ru>
2026-06-11 01:54:02 +0300

    hotfix styles

--- src/shared/widgets/project-carousel/url-slide.tsx
+++ src/shared/widgets/project-carousel/url-slide.tsx
@@ -26,7 +26,7 @@ export function UrlSlide({
       <div className="relative inline-block group rounded-[0.5rem] overflow-hidden">
         <img
           className={cn("embla__slide__img", {
-            "border-4 border-primary": isMain,
+            "border-4 border-primary": isEditable && isMain,
           })}
           src={url}
           alt={`Изображение проекта ${index + 1}`}
@@ -37,15 +37,17 @@ export function UrlSlide({
           }}
         />
 
-        <div
-          className={cn(
-            "absolute inset-0 bg-black/30 opacity-0 transition-opacity duration-300 group-hover:opacity-100",
-            {
-              "opacity-100": isMobile,
-              "group-hover:opacity-100": !isMobile,
-            }
-          )}
-        />
+        {isEditable && (
+          <div
+            className={cn(
+              "absolute inset-0 bg-black/30 opacity-0 transition-opacity duration-300 group-hover:opacity-100",
+              {
+                "opacity-100": isMobile,
+                "group-hover:opacity-100": !isMobile,
+              }
+            )}
+          />
+        )}
         {isEditable && (
           <div
             className={cn(
